f359c7ed274da04129fa67191d3f2a34b9c00b01
dehansa-dissa <deha.dissa@gmail.com>
2025-12-24 23:00:32 +0530

    Hero section created with search bar

---
+++ movie-website/src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = ({searchItem, setSearchItem}) => {
+  return (
+    <div className="search">
+        <div>
+            <img src='/search.svg' alt='search icon' />
+            <input 
+            type="text"
+            placeholder="Search for a movie..."
+            value={searchItem}
+            onChange={(e) => setSearchItem(e.target.value)}
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Search

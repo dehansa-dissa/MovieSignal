b90a8c6b73f1dce02233669d40c53a2c41060279
dehansa-dissa <deha.dissa@gmail.com>
2026-05-04 20:26:56 +0530

    added description to movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const MovieCard = ({movie: { 
-    title ,
-    vote_average ,
-    poster_path ,
-    release_date ,
-    original_language ,
-}}) => {
+const MovieCard = ({movie}) => {
+  // Use TMDB's 'overview' as description fallback
+  const {
+    title,
+    vote_average,
+    poster_path,
+    release_date,
+    original_language,
+    description,
+    overview
+  } = movie;
+  const movieDescription = description || overview;
   return (
     <div className="movie-card">
         <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'} alt={`${title} Poster`} />
@@ -14,15 +19,17 @@ const MovieCard = ({movie: {
         <h3 className='mt-4'>{title}</h3>
 
         <div className='content'>
-            <div className='rating'>
-                <img src='/star.svg' alt='star icon' />
-                <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-                <span>•</span>
-                <p className='lang'>{original_language}</p>
-                <span>•</span>
-                <p>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
-
-            </div>
+          {movieDescription && (
+            <p className='movie-description'>{movieDescription}</p>
+          )}
+          <div className='rating'>
+            <img src='/star.svg' alt='star icon' />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+            <span>•</span>
+            <p className='lang'>{original_language}</p>
+            <span>•</span>
+            <p>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+          </div>
         </div>
     </div>
   )
